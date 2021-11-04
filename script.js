1fedf555b56729108a65a58c85df561454f0430a
laszlo.t.flora@gmail.com <laszlo.t.flora@gmail.com>
2021-11-04 22:15:56 +0100

    position tricks

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
 function loadEvent() {
-    // console.log('The page has loaded');
+
+    //    function amountscrolled(){
+    //     var winheight= window.innerHeight;
+    //     var docheight = getDocHeight();
+    //     var scrollTop = window.pageYOffset;
+    //     var trackLength = docheight - winheight;
+    //     var pctScrolled = Math.floor(scrollTop/trackLength * 100); 
+    //     // gets percentage scrolled (ie: 80 or NaN if tracklength == 0)
+    //     console.log("valami");
+    //     console.log(pctScrolled + '% scrolled');
+    // }
+    
+    // window.addEventListener("scroll", function(){
+    //     amountscrolled()
+    // }, false);
+    
+    
+    let link = document.getElementById("link1");
+    link.addEventListener("click", function() {
+        let image = document.querySelectorAll("#img1");
+        console.log(image);
+        for (const key in image) {
+            image[key].classList = ("selected");
+        }
+    });
+
+    
 }
 
 window.addEventListener("load", loadEvent);
